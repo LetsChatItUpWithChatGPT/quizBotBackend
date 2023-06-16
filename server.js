ab909d6f52916a5daed3bc54b1a237f0e418a753
Kao Saelor <saelor.kao@gmail.com>
2023-06-15 19:32:05 -0700

    clean up code

--- server.js
+++ server.js
@@ -1,108 +1,69 @@
 'use strict';
 
 require('dotenv').config();
+
 const { App } = require('@slack/bolt');
-const { Configuration, OpenAIApi } = require('openai');
+const axios = require('axios');
 
-const userConversations = new Map();
-
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-
-const openAI = new OpenAIApi(configuration);
+const classData = require('./quizData.json');
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
-  socketMode: true,
-  appToken: process.env.SLACK_APP_TOKEN,
 });
 
-// !! PROOF OF LIFE MESSAGE
-app.message('hello', async ({ message, say }) => {
-  await say('Hello, world!');
-});
-
-const chatModule = (app) => {
-  app.event('message', async ({ event, ack, say }) => {
-    // Check if the message is from the QuizBot channel
-    if (event.channel === 'U05CJQQ05PC') { // Replace 'C12345678' with the actual ID of the QuizBot channel
-      const channelId = event.channel;
-
-      let conversationHistory = userConversations.get(channelId) || [
-        { role: 'system', content: 'Provide basic response to questions without code unless requested by user.' },
-      ];
-
-      if (event.channel_type === 'im') {
-        console.log(event.text);
-        console.log('event>>>>>', event);
-
-        conversationHistory.push({ role: 'user', content: event.text });
-
-        const userMessage = event.text;
-        const modifiedUserMessage = `Steps to solve this problem domain: ${userMessage}`;
-        conversationHistory.push({ role: 'user', content: modifiedUserMessage });
-
-        say({
-          text: ':robot_face: AI is formulating a response...',
-        });
-
-        const response = await openAI.createChatCompletion({
-          model: 'gpt-3.5-turbo',
-          temperature: 0.8,
-          messages: conversationHistory,
-          stream: true,
-          max_tokens: 150,
-        });
-
-        console.log('conversationHistory>>>>>', conversationHistory);
-
-        const aiResponse = response.data.choices[0].message.content;
-
-        conversationHistory.push({ role: 'assistant', content: aiResponse });
-
-        userConversations.set(channelId, conversationHistory);
-
-        console.log(response.data.choices);
-        say({
-          text: aiResponse,
-        });
-
-        if (conversationHistory.length > 0) {
-          const isFollowUpQuestion = userMessage.includes('Further explanation');
-          if (isFollowUpQuestion) {
-            const followUpQuestion = userMessage.substring(userMessage.indexOf(':') + 1).trim();
-            conversationHistory.push({ role: 'user', content: followUpQuestion });
-
-            const followUpResponse = await openAI.createChatCompletion({
-              model: 'gpt-3.5-turbo',
-              temperature: 0.8,
-              messages: conversationHistory,
-            });
-
-            const aiFollowUpResponse = followUpResponse.data.choices[0].message.content;
-            conversationHistory.push({ role: 'assistant', content: aiFollowUpResponse });
-
-            userConversations.set(channelId, conversationHistory);
-
-            console.log(followUpResponse.data.choices);
-            say({
-              text: aiFollowUpResponse,
-            });
-          }
-        }
+async function generateQuizQuestion(topic) {
+  try {
+    const response = await axios.post(
+      'https://api.openai.com/v1/engines/davinci-codex/completions',
+      {
+        prompt: `Generate a quiz question for ${topic}`,
+        max_tokens: 50,
+        temperature: 0.8,
+        n: 1,
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+        },
       }
-    }
-  });
-};
+    );
 
-chatModule(app);
+    const question = response.data.choices[0].text.trim();
+    return question;
+  } catch (error) {
+    console.error('Error generating quiz question:', error.message);
+    return 'Sorry, an error occurred while generating the quiz question.';
+  }
+}
+
+app.command('/quiz', async ({ command, ack, say }) => {
+  try {
+    await ack();
+
+    const selectedTopic = command.text.toLowerCase();
+
+    const classInfo = classData.find(
+      (info) => info.topic.toLowerCase() === selectedTopic
+    );
+
+    if (classInfo) {
+      const quizQuestion = await generateQuizQuestion(classInfo.topic);
+      const message = `Here's a quiz question for ${classInfo.topic}: ${quizQuestion}`;
+      await say(message);
+    } else {
+      const errorMessage = 'Sorry, I couldn\'t find any matching class for the selected topic.';
+      await say(errorMessage);
+    }
+  } catch (error) {
+    console.error('Error processing /quiz command:', error.message);
+    await say('Sorry, an error occurred while processing your request.');
+  }
+});
 
 (async () => {
   const port = process.env.PORT || 3002;
   await app.start(port);
   console.log(`⚡️ Slack Bolt app is running on port ${port}!`);
 })();
-
-module.exports = chatModule;
